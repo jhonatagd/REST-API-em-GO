b9473f6afa461b190c95909ee36ae10739118da4
Jhonata Godoy <88396979+jhonatagd@users.noreply.github.com>
2025-01-24 21:55:48 -0300

    adjust commit

--- api/main.go
+++ api/main.go
@@ -7,6 +7,28 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+type User struct{
+	ID int `json: "id`
+	Name string `json: "name`
+}
+
 func getUsers(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+
+	w.Header().Set("Content=Type", "application/json")
+	json.NewEncoder(w).Encode([User{
+		{
+			ID:1,
+			Name: "Jhonata",
+		},
+		{
+			ID:2,
+			Name: "Godoy",
+		},
+	}])
 }
